fd1bb2a93c12fe41495569ea235179dc5e01b606
vtadevosian <vahram_tadevosyan@edu.aua.am>
2020-06-11 21:37:30 +0400

    Initial Commit

---
+++ WeatherNow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherNow
+//
+//  Created by Vahram Tadevosian on 6/11/20.
+//  Copyright © 2020 Vahram Tadevosian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
